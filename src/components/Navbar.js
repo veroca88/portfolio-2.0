02835cd2623910f7bbd735b18f1d0144f305a44c
veroca88 <veronicadonoso1506@gmail.com>
2020-09-13 23:00:02 -0400

    Refactor navbar, design and funtionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,66 @@
-import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 
-export default function Navig() {
+export default function Navig({ color }) {
+  const [navOpen, setNavOpen] = useState(false);
+
+  function closeNav() {
+    setNavOpen(!navOpen);
+  }
+
   return (
-    <>
-      <Navbar className="nav" expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link
-              activeClass="active"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="aboutMe"
-              className="text-secondary"
-            >
-              About me!
-            </Link>
-            <Link
-              activeClass="active"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="myWork"
-              className="text-secondary"
-            >
-              My Work
-            </Link>
-            <Link
-              activeClass="active"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-              to="contactMe"
-              className="text-secondary"
-            >
-              Keep in Touch
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <nav className={`responsive-nav ${color}`}>
+      <figure onClick={() => setNavOpen(!navOpen)}>
+        <i className="fas fa-server fa-2x logo-nav"></i>
+      </figure>
+      <ul
+        style={{
+          backgroundColor: color === "dark-mode mode" ? "#2a2c31" : " #fffff7",
+        }}
+      >
+        <li className={navOpen ? "" : "li-desactive"}>
+          <Link
+            onClick={closeNav}
+            // activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="aboutMe"
+            className="navbar-link"
+          >
+            About me!
+          </Link>
+        </li>
+        <li className={navOpen ? "" : "li-desactive"}>
+          <Link
+            onClick={closeNav}
+            // activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="myWork"
+            className="navbar-link"
+          >
+            My Work
+          </Link>
+        </li>
+        <li className={navOpen ? "" : "li-desactive"}>
+          <Link
+            onClick={closeNav}
+            // activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-70}
+            duration={500}
+            to="contactMe"
+            className="navbar-link"
+          >
+            Keep in Touch
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
